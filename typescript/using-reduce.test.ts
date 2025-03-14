7f615bf6a7f1200a2329edd8f2ad9cb2865f3645
Jose Castro <jose.castro@apiumhub.com>
2025-03-14 13:09:24 +0100

    copy of the forEach

--- typescript/using-reduce.test.ts
+++ typescript/using-reduce.test.ts
@@ -1,10 +1,30 @@
+require('./using-reduce')
 
+let myArray = [1, 2, 3]
+
+// myArray.forEach((item) => {
+//   item + 1;
+// })
+
+function customForEach<T>(arr: T[], callback: (v: T, i: number) => void) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], i)
+  }
+  // arr.reduce((acc, curr, i) => {
+  //   callback(curr, i)
+  //   return undefined;
+  // }, undefined)
+}
 
 describe('Using reduce', () => {
-  it('should sum all numbers in an array', () => {
-    const numbers = [1, 2, 3, 4, 5];
-    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-
-    expect(sum).toBe(15);
-  });
+  describe('For Each', () => {
+    it('[1, 2, 3] should return [2, 3, 4]', () => {
+      customForEach(myArray, (item: number, i: number) => {
+        item += 1;
+        myArray[i] = item
+      })
+      console.log('myArray', myArray)
+      expect(myArray).toEqual([2, 3, 4]);
+    });
+  }) 
 });
---
+++ typescript/using-reduce.ts
@@ -0,0 +1,3 @@
+export function customForEach() {
+  return 0;
+}
